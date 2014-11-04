556c7391a801a24b06a2600c96bc0bdf14666686
Marius Gedminas <marius@gedmin.as>
2014-11-04 19:26:43 +0200

    Back to development: 4.0.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ long_description = (open('README.txt').read() + '\n\n' +
                     open('CHANGES.txt').read())
 
 setup(name='zope.browserresource',
-      version='4.0.2',
+      version='4.0.3.dev0',
       url='http://pypi.python.org/pypi/zope.browserresource/',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
