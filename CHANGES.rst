86bb020ec527523cce5fa79e7ce599b421c183b1
Michael Howitz <icemac@gmx.net>
2025-02-14 11:14:09 +0100

    Preparing release 5.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changes
 =========
 
-5.2 (unreleased)
+5.2 (2025-02-14)
 ================
 
 - Add support for Python 3.12, 3.13.
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ TESTS_REQUIRE = ZCML_REQUIRES + [
 ]
 
 setup(name='zope.browserresource',
-      version='5.2.dev0',
+      version='5.2',
       url='https://github.com/zopefoundation/zope.browserresource/',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
