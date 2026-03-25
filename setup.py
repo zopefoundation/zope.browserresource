a564b30b22e1d2c6bd16796ac3d58356dc0c5bfe
Michael Howitz <icemac@gmx.net>
2026-03-25 09:02:20 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -13,74 +13,8 @@
 ##############################################################################
 """zope.browserresource setup
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-long_description = read('README.rst') + '\n\n' + read('CHANGES.rst')
-
-ZCML_REQUIRES = [
-    'zope.component[zcml]',
-    'zope.security[zcml] >= 3.8',
-]
-
-TESTS_REQUIRE = ZCML_REQUIRES + [
-    'zope.testing',
-    'zope.testrunner >= 6.4',
-]
-
-setup(name='zope.browserresource',
-      version='6.1.dev0',
-      url='https://github.com/zopefoundation/zope.browserresource/',
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.dev',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'Programming Language :: Python :: 3.13',
-          'Programming Language :: Python :: 3.14',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope :: 3',
-      ],
-      description='Browser resources implementation for Zope.',
-      long_description=long_description,
-      license='ZPL-2.1',
-      include_package_data=True,
-      python_requires='>=3.10',
-      install_requires=[
-          'setuptools',
-          'zope.component>=3.8.0',
-          'zope.configuration',
-          'zope.contenttype>=4.0.1',
-          'zope.i18n',
-          'zope.interface',
-          'zope.location',
-          'zope.publisher>=3.8',
-          'zope.schema',
-          'zope.traversing>3.7',
-      ],
-      extras_require={
-          'test': TESTS_REQUIRE,
-          'zcml': ZCML_REQUIRES,
-          'docs': [
-              'Sphinx',
-              'repoze.sphinx.autointerface',
-          ],
-      },
-      zip_safe=False)
+# See pyproject.toml for package metadata
+setup()
